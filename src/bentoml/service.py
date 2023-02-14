a56f3488aa95a069ea44faa25f249a0c26feb798
ballchuuu <32189828+ballchuuu@users.noreply.github.com>
2023-02-14 14:20:16 +0800

    Add BentoML service for model prediction (#3)
    * update gitignore file
    
    * shift model assets to bentoml folder
    
    * add code to pack chosen model weights
    
    * add bentoml service
    
    * update notebook code and clean up
    
    ---------
    
    Co-authored-by: root <root@vinnie.localdomain>
    Co-authored-by: Vinnie <vinniechuyixuan@gmail.com>

---
+++ src/bentoml/service.py
@@ -0,0 +1,13 @@
+import bentoml
+import numpy as np
+from bentoml.io import NumpyNdarray
+
+from config import settings
+
+runner = bentoml.sklearn.get(f"{settings.bento_model_name}:latest").to_runner()
+svc = bentoml.Service(settings.bento_model_name, runners=[runner])
+
+@svc.api(input=NumpyNdarray(), output=NumpyNdarray())
+def classify(input_series: np.ndarray) -> np.ndarray:
+    result = runner.predict.run(input_series)
+    return result
