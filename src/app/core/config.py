4cc93e09dbc5f323716ce7914cbff31c487f48c6
Vinnie <vinniechuyixuan@gmail.com>
2023-02-14 15:43:58 +0800

    add dockerfile and fix code for deployment

--- src/app/core/config.py
+++ src/app/core/config.py
@@ -13,6 +13,8 @@ class Settings(BaseSettings):
     # Model Details
     ###
     pickle_model_name = "log_reg_1"
+    # for deploying of both bento and fastapi services locally via docker, use:
+    # "http://host.docker.internal:3000/classify"
     bento_server = "http://localhost:3000/classify"
 
 
