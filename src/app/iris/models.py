c8a995518af12d1fd7c0797bedf9ca6110699bce
Vinnie <vinniechuyixuan@gmail.com>
2023-02-14 14:22:28 +0800

    add main iris endpoint

---
+++ src/app/iris/models.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+
+###
+# Iris model input
+###
+class InputIris(BaseModel):
+    SepalLengthCm: float
+    SepalWidthCm: float
+    PetalLengthCm: float
+    PetalWidthCm: float
+
+###
+# Iris model output
+###
+class OutputIris(BaseModel):
+    output: str
+    model_name: str
+
+###
+# Logging
+###
+class IrisInfoLog(BaseModel):
+    param: InputIris
+    response: OutputIris
+
+class IrisErrorLog(BaseModel):
+    param: InputIris
+    error_msg : str
+
