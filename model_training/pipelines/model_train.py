72c3a04aec38eb455a45e52bc1a5647459dc5d2e
ballchuuu <32189828+ballchuuu@users.noreply.github.com>
2023-02-15 11:06:50 +0800

    Add model pipelines (#7)
    * shift notebooks under model training folder
    
    * add pipelines for model training based on notebooks
    
    * lint
    
    ---------
    
    Co-authored-by: Vinnie <vinniechuyixuan@gmail.com>

---
+++ model_training/pipelines/model_train.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from utils import train_model
+from utils import train_test_split_by_species
+
+if __name__ == "__main__":
+    df = pd.read_csv("../../data/Iris.csv")
+
+    # Train test split
+    X = df.drop(["Species", "Id"], axis=1)
+    y = df["Species"]
+    X_train, X_test, y_train, y_test = train_test_split_by_species(
+        X=X, y=y, test_size=0.25
+    )
+
+    # initialise model
+    model = LogisticRegression()
+
+    # train model and save weights to pickle file
+    model = train_model(
+        X_train=X_train,
+        X_test=X_test,
+        y_train=y_train,
+        y_test=y_test,
+        model=model,
+        model_name="log_reg"
+    )
